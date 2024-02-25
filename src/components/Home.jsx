37797846bec56609c42aebfb313344ecfef44732
EmonThePrince <saimahmedemon7@gmail.com>
2024-02-25 18:48:26 +0600

    updated homepage. yet to do the blur in the search area

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
 
         <div className="h-screen">
             <div className="flex justify-center w-screen items-center fixed top-24 left-auto"> <SearchBar /></div>
-            <div className="text-9xl flex justify-center items-center h-full"> Home home home home Home home home home Home home home home Home home home home Home home home home </div>
+            
             
         </div>
 
